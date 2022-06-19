9aebdcd5c1d7f027aec04d028d9fbb1849a20bd6
He1senberg31415 <n1clashein118@gmail.com>
2022-06-19 21:33:21 +0200

    init

--- clientTest.py
+++ clientTest.py
@@ -1,7 +1,7 @@
 import socket
 
 udpip = "de1.localtonet.com"
-udpport = 44046
+udpport = 50708
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.sendto(b"test", (udpip, udpport))
--- serverTest.py
+++ serverTest.py
@@ -1,7 +1,7 @@
 import socket
 
 UDP_IP = "localhost"
-UDP_PORT = 5001
+UDP_PORT = 55555
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind((UDP_IP, UDP_PORT))
 while True:
